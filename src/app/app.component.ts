ea83ab3e51231d97bd40bb5caedd6c54df26b67c
AkashSenta <akashsenta@hotmail.com>
2019-09-11 00:00:14 +0530

    angular first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { Todo } from './todo';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'youtube-app';
+  title = 'ToDo Demo';
+
+  todoValue: string;
+  list: Todo[];
+
+  ngOnInit() {
+    this.list = [];
+    this.todoValue = "";
+  }
+
+  /* Add item function */
+  addItem() {
+    if (this.todoValue !== "") {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = "";
+  }
+ 
+  /* Delete item */
+  deleteItem(id:number) {
+    this.list = this.list.filter(item=> item.id !==id);
+  }
+
 }
